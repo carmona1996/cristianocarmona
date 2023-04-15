df4a42dfcf9ac02567b4ac166ce2917ca6100886
carmona1996 <130859515+carmona1996@users.noreply.github.com>
2023-04-15 14:19:44 -0300

    Add files via upload

---
+++ JavaApplication6/build/generated-sources/ap-source-output/modelo/basico/Usuario_.java
@@ -0,0 +1,15 @@
+package modelo.basico;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.10.v20211216-rNA", date="2023-04-15T11:38:25")
+@StaticMetamodel(Usuario.class)
+public class Usuario_ { 
+
+    public static volatile SingularAttribute<Usuario, String> nome;
+    public static volatile SingularAttribute<Usuario, Long> id;
+    public static volatile SingularAttribute<Usuario, String> email;
+
+}
